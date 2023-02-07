8e1f7eb99f51bea998967b186b4daa67165fdf2d
Irving Rivera <irvrivera67@gmail.com>
2023-02-07 13:23:10 -0500

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-horizontal-00901",
     port: 5432,
     user: "postgres",
     password: "test",
